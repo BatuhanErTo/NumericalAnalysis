7bacad87785f125c820453fd93600e17180a9c93
Batuhan Erol <46936547+BatuhanErTo@users.noreply.github.com>
2023-04-16 02:18:53 +0300

    Create week4_example_1.m

---
+++ week4_example_1.m
@@ -0,0 +1,8 @@
+syms c
+
+f(c) = 9.8*68.1*(1-exp(-10*c/68.11))/c-40;
+real_root = double(solve(f(c)))
+
+g = inline("9.8*68.1*(1-exp(-10*c/68.11))/c-40");
+real_root2 = fzero(g,[12,16])
+real_root3 = fzero("coefficient",[12,16])
