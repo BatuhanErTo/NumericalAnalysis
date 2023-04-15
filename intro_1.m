3c5ce8d6c47aae40c3e3bf0afae2af8b155bfdad
Batuhan Erol <46936547+BatuhanErTo@users.noreply.github.com>
2023-04-15 15:23:26 +0300

    Create intro_1.m

---
+++ intro_1.m
@@ -0,0 +1,23 @@
+a = 4;
+b = 86;
+% noktalı virgül kullanılan yerler outputta gözükmeyecek
+a/(b*b)
+
+a = 'erü';
+
+x = [1 2 3 4 5]
+y = [2.4 5.6 7 8.2 12.3]
+
+matrix = [4 3 7; 1.2 4 5.3] % 2x3 matris oluşturma
+
+plot(x,y)
+xlabel('x değerleri')
+ylabel('y değerleri')
+title('f(x,y) değerleri')
+
+value = input("veri al:")
+
+string_value = input('veri al','s')
+
+disp('Erciyes Bilgisayar')
+disp(2.45)
